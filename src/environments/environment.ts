b4762ccef706727c69b5eccaa3dfb228d5024c0b
Sky <jinghao0958@gmail.com>
2024-11-06 22:43:40 +0800

    config firebase and change readme file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,13 @@ export const environment = {
   production: false,
   defaultauth: 'fackbackend',
   firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: "AIzaSyCyQiaqIWRZz-F0NS9390OPV-L4jpNhV4o",
+    authDomain: "trackhub-558c7.firebaseapp.com",
+    projectId: "trackhub-558c7",
+    storageBucket: "trackhub-558c7.firebasestorage.app",
+    messagingSenderId: "372996017768",
+    appId: "1:372996017768:web:fc0dbfe4c983b2c1e135dd",
+    measurementId: "G-3BF3DWL7SM"
   }
 };
 
